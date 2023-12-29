9af12733fbc8c0e89fafb79e03f2ed496881f8c1
Anastasiia Sobkevych <anastasia.sobkevych@shelf.io>
2023-12-30 00:57:05 +0200

    add component init

---
+++ component/__init__.py
@@ -0,0 +1,36 @@
+import os
+import streamlit.components.v1 as components
+import streamlit as st
+
+_RELEASE = True
+
+if _RELEASE:
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(root_dir, 'frontend/build')
+
+    _copilot = components.declare_component(
+        "Copilot",
+        path=build_dir
+    )
+else:
+    _copilot = components.declare_component(
+        "Copilot",
+        url="http://localhost:3001"
+    )
+
+
+def copilot(prompt_template, api_url, rpm_limit=100, height: int = 100, font_family: str = 'Helvetica', **model_kwargs):
+    return _copilot(
+        prompt_template=prompt_template,
+        api_url=api_url,
+        rpm_limit=rpm_limit,
+        height=height,
+        font_family=font_family,
+        default=None,
+        **model_kwargs
+    )
+
+
+if not _RELEASE:
+    value = copilot(prompt_template='{text}', api_url="https://trusty-humble-jackal.ngrok-free.app/v1/completions", rpm_limit=20, max_tokens=10, stop=["\n", '.'])
+    st.write(f"Your text: {value}")
